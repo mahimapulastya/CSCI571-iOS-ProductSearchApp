f994e30a975ba7f9c9d6fc1bf35e4375af71906f
Mahima Pulastya <pulastya@usc.edu>
2019-04-07 02:57:55 -0700

    Initial Commit

---
+++ productsearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  productsearch
+//
+//  Created by Mahima Pulastya on 4/7/19.
+//  Copyright © 2019 Mahima Pulastya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
