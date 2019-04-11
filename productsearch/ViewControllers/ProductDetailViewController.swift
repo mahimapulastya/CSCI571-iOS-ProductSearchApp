e3cc1405595310458c93aa0f5c70b444c1b9814c
Mahima Pulastya <pulastya@usc.edu>
2019-04-10 23:24:29 -0700

    go to product details

---
+++ productsearch/ViewControllers/ProductDetailViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ProductDetailViewController.swift
+//  productsearch
+//
+//  Created by Mahima Pulastya on 4/10/19.
+//  Copyright © 2019 Mahima Pulastya. All rights reserved.
+//
+
+import Foundation
